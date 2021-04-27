a21a40aca2bee8b16d85f9065c7f6dff540d8532
Cli <christyinman.tech@gmail.com>
2021-04-27 10:18:28 -0600

    testing started

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Campspot-Challenge'`, () => {
+  it(`should have as title 'Campspot "Gap Rule" Challenge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Campspot-Challenge');
+    expect(app.title).toEqual('Campspot "Gap Rule" Challenge');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Campspot-Challenge app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Campspot "Gap Rule" Challenge');
   });
 });
